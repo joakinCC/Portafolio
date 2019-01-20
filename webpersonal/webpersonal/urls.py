8b45081c9515253138d312458ee6d27a7ba0bd60
Joaquín Casanova <joaquin.casanova.c@gmail.com>
2019-01-19 18:01:15 -0600

    cambiando a espaniol y personalizando administrado

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -24,3 +26,8 @@ urlpatterns = [
     path('portfolio/', views.porfolio, name='portfolio'),
     path('contact/', views.contact, name='contact'),
 ]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
